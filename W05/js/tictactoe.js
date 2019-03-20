60d796280f8b98d8f7aab246069763f60770ee25
pychen531 <pychen531@gms.tku.edu.tw>
2019-03-20 15:00:32 +0800

    W05 files

---
+++ W05/js/tictactoe.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+    let turn = 0; //even for player 'o', odd for player 'x'
+
+    $('#board li').on('click', function () {
+        if ($(this).hasClass('disable')) {
+            alert('Already filled.');
+        } else if (turn % 2 == 0) {
+            $(this).text('O');
+            $(this).addClass('disable o')
+        } else if (turn % 2 == 1) {
+            $(this).text('X');
+            $(this).addClass('disable x')
+        }
+        turn++;
+    })
+
+    $.fn.reset = function () {
+        $('#board li').text('+');
+        $('#board li').removeClass('o');
+        $('#board li').removeClass('x');
+        $('#board li').removeClass('disable');
+    }
+
+
+    $('#reset').on('click', function () {
+        $.fn.reset();
+    })
+})
